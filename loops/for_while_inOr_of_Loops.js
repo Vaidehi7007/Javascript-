5250039ecff6ab4d12f9be5b7ccc8e4f57604fec
CODER_VAIDEHI <142117207+Vaidehi7007@users.noreply.github.com>
2023-12-27 05:44:22 +0000

    for,while,in or of loops

---
+++ loops/for_while_inOr_of_Loops.js
@@ -0,0 +1,36 @@
+console.log("I am a tutorial on Loops")
+
+let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) { 
+//     console.log(a + i); 
+// }
+
+// let obj = {
+//     name: "Himanshu",
+//     role: "Programmer",
+//     company: "Amazon"
+// }
+ 
+// for (const key in obj) {  
+//         console.log(key)
+// }
+
+// for (const c of "Himanshu") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<60000) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
